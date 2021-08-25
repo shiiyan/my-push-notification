8225a394e5fe1c42362e1f26f1cb4b9cb6d8f584
shiiyan <yan39123@gmail.com>
2021-08-25 22:36:20 +0900

    push notificaiton from local

---
+++ index.js
@@ -0,0 +1,39 @@
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker Support!");
+  }
+
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support");
+  }
+};
+
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register("service.js");
+  return swRegistration;
+};
+
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission();
+  if (permission !== "granted") {
+    throw new Error("Permission not granted for notification");
+  }
+
+  return permission;
+};
+
+const showLocalNotification = (title, body, swRegistration) => {
+  const options = {
+    body,
+  };
+  swRegistration.showNotification(title, options);
+};
+
+const main = async () => {
+  check();
+  const swRegistration = await registerServiceWorker();
+  const permission = await requestNotificationPermission();
+  showLocalNotification("This is title", "This is message", swRegistration);
+};
+
+main();
