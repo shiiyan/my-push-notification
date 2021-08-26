4cfc44d2f691873110752b737ef4e9a578bb9068
shiiyan <yan39123@gmail.com>
2021-08-26 11:30:50 +0900

    add save subscription to server at service.js

--- service.js
+++ service.js
@@ -1,9 +1,46 @@
+const urlB64ToUint8Array = (base64String) => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+
+const saveSubscription = async (subscription) => {
+  const SERVER_URL = "http://localhost:4000/save-subscription";
+  const response = await fetch(SERVER_URL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(subscription),
+  });
+  return response.json();
+};
+
 self.addEventListener("activate", async () => {
   try {
-    const options = {};
+    const applicationServerKey = urlB64ToUint8Array(
+      "BPlvxdwm6nlHm5t4y8Zay5NR4OfR-OJJSDCd-gi_oo94K8MN8k-iaOUQUTC-FK3M-hlpfB-W2xoNUNyDCXp1Sjc"
+    );
+    const options = { applicationServerKey, userVisibleOnly: true };
     const subscription = await self.registration.pushManager.subscribe(options);
-    console.log(JSON.stringify(subscription));
+    const response = await saveSubscription(subscription);
+    console.log(response);
   } catch (err) {
     console.log("Error: ", err);
+  }
+});
+
+self.addEventListener("push", (event) => {
+  if (event.data) {
+    console.log("Push event!! ", event.data.text());
+  } else {
+    console.log("Push event but no data");
   }
 });
